9ad3d0057680bd2cd093b1379611ad2e5d641708
Nekrons <antivenomstreet@gmail.com>
2024-10-01 05:42:31 +0300

    Initial commit

--- src/main/kotlin/org/belafu/kotlincourse/lesson6/Homework.kt
+++ src/main/kotlin/org/belafu/kotlincourse/lesson6/Homework.kt
@@ -1,12 +1,16 @@
 package org.belafu.kotlincourse.lesson6
 
+import java.lang.Character.UnicodeScript
+
 fun main ()  {
 println(season(month.toByte())) // Не совсем понял почему тут to Byte добавилось, но программа сама его вставила обозначая отсуствие как ошибку.
     println(move(length))
     println(dogage(age))
+    println(score(scrip))
     println(type(filet))
     println(temp(weather))
     println(kage(kidsage))
+
 }
 
 val month = 6
@@ -32,6 +36,14 @@ fun dogage (age: Double): Double {
    return if (age in 0.0..2.0) age * 10.5
     else (age - 2) * 4 + 21
 }
+
+val scrip = 1301
+fun score (scrip: Int): Any {
+return if (scrip <= 99) "0"
+    else if (scrip in 100..1000) scrip / 100 * 2
+    else ((scrip -1000) / 100 * 5 +20)
+}
+
 
 val filet = "png"
 fun type (filet: String): String {
